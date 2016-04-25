ce00022c33a915e9a0a88b1e32ee1a81f24d36da
Shrralis <Zolotorig@i.ua>
2016-04-25 06:22:52 +0300

    practically finished lab_1_7 (todo: point33)

---
+++ src/lab_1_7/StudentInfo.java
@@ -0,0 +1,35 @@
+package lab_1_7;
+
+/**
+ * Created by Shrralis on 4/25/2016.
+ */
+public class StudentInfo {
+
+    private float minimal; // мінімальний бал
+    private float average;  // середній бал
+    private float maximal; // маскимальний бал
+
+    public float getMinimal() {
+        return minimal;
+    }
+
+    public void setMinimal(float minimal) {
+        this.minimal = minimal;
+    }
+
+    public float getAverage() {
+        return average;
+    }
+
+    public void setAverage(float average) {
+        this.average = average;
+    }
+
+    public float getMaximal() {
+        return maximal;
+    }
+
+    public void setMaximal(float maximal) {
+        this.maximal = maximal;
+    }
+}
--- src/lab_1_7/StudentsMap.java
+++ src/lab_1_7/StudentsMap.java
@@ -9,9 +9,87 @@ import java.util.Map;
 public class StudentsMap {
 
     public static void main(String[] args) {
-        Map<String, Double> students = new HashMap<String, Double>();
-        students.put("Жиравов", 8.1);
-        students.put("Пастушенко", 6.6);
-        students.put("Рибак", 6.1);
+        Map<String, Float> students = new HashMap<String, Float>();
+        students.put("Жиравов", 8.1f);
+        students.put("Пастушенко", 6.6f);
+        students.put("Рибак", 6.1f);
+        showMap(students);
+
+        Map<String, StudentInfo> studentsInfo = new HashMap<String, StudentInfo>();
+
+        StudentInfo info = new StudentInfo();
+        info.setMinimal(7);
+        info.setAverage((float) 8.4);
+        info.setMaximal(10);
+        studentsInfo.put("Вася Пупкін", info);
+
+        StudentInfo[] sInfo = new StudentInfo[3];
+        for (int i = 0; i < sInfo.length; i++) {
+            sInfo[i] = new StudentInfo();
+        }
+        sInfo[0].setMinimal(4);
+        sInfo[0].setAverage(6.6f);
+        sInfo[0].setMaximal(8);
+        studentsInfo.put("Пастушенко Василь", sInfo[0]);
+        sInfo[1].setMinimal(4);
+        sInfo[1].setAverage(6.1f);
+        sInfo[1].setMaximal(7);
+        studentsInfo.put("Рибак Сергій", sInfo[1]);
+        sInfo[2].setMinimal(4);
+        sInfo[2].setAverage(8.1f);
+        sInfo[2].setMaximal(11);
+        studentsInfo.put("Жиравов Ярослав", sInfo[2]);
+
+        for (String student :
+                studentsInfo.keySet()) {
+            System.out.println(student + " min: " + showStudentInfo(studentsInfo.get(student), "min"));
+        }
+
+        System.out.println();
+        for (String student :
+                studentsInfo.keySet()) {
+            System.out.println(student + " max: " + showStudentInfo(studentsInfo.get(student), "max"));
+        }
+
+        System.out.println();
+        for (String student :
+                studentsInfo.keySet()) {
+            float avgMark = Float.parseFloat(showStudentInfo(studentsInfo.get(student), "avg").trim());
+            if(avgMark >= 7.0f) {
+                System.out.println(student + " avg: " + avgMark);
+            }
+        }
+
+        System.out.println();
+        for (String student :
+                studentsInfo.keySet()) {
+            float realAvgMark = Float.parseFloat(showStudentInfo(studentsInfo.get(student), "avg").trim());
+            float avgMark = (Float.parseFloat(showStudentInfo(studentsInfo.get(student), "max").trim()) +
+                    Float.parseFloat(showStudentInfo(studentsInfo.get(student), "min").trim())) / 2;
+            System.out.println(student + " realAvg: " + realAvgMark + " - computedAvg: " + avgMark + " - " +
+                    "difference (cAvg-realAvg): " + (avgMark - realAvgMark));
+        }
     }
+
+    static void showMap(Map map) {
+        for (Object o :
+                map.keySet()) {
+            System.out.println(o + " - " + (map.get(o).getClass().getName().matches("^.*StudentInfo$") ?
+                    showStudentInfo((StudentInfo) map.get(o)) : map.get(o)));
+        }
+    }
+
+    static String showStudentInfo(StudentInfo studentInfo) {
+        return (studentInfo.getMinimal() + " - " +
+                studentInfo.getAverage() + " - " +
+                studentInfo.getMaximal());
+    }
+
+    static String showStudentInfo(StudentInfo studentInfo, String mode) {
+        return ((mode.matches("^.*min.*$") ? studentInfo.getMinimal() : "") + "" +
+                (mode.matches("^.*avg.*$") ? studentInfo.getAverage() : "") + "" +
+                (mode.matches("^.*max.*$") ? studentInfo.getMaximal() : ""));
+    }
+
+    //TODO: make point 33!!!
 }
